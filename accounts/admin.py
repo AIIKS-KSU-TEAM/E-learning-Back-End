0bd39f5200b52dfa20b73291cfd071581b4af596
Azenga Kevin <azenga.kevin7@gmail.com>
2024-10-21 08:58:05 +0300

    wip: update admin for the accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from .models import UserProfile
+from accounts.models import UserProfile, User
 
-admin.site.register(UserProfile)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    ]
+
+    list_filter = [
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    ]
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "phone_number"]
