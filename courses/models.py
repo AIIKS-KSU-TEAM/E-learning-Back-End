e948a0168525a4ff83d8261f1ba270279da86d43
Azenga Kevin <azenga.kevin7@gmail.com>
2024-10-21 15:29:01 +0300

    wip: course models refactor and more

--- courses/models.py
+++ courses/models.py
@@ -2,31 +2,47 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django_extensions.db.models import (
+    TimeStampedModel,
+    TitleSlugDescriptionModel,
+    TitleDescriptionModel,
+)
+from courses.fields import OrderField
 
 # Get the user model to link to the owner field
 User = get_user_model()
 
 
+class Subject(TitleSlugDescriptionModel, TimeStampedModel):
+    pass
+
+
 # Course Model
-class Course(models.Model):
+class Course(TitleSlugDescriptionModel, TimeStampedModel):
     owner = models.ForeignKey(
-        User, related_name="courses_created", on_delete=models.CASCADE
+        User,
+        related_name="courses_created",
+        on_delete=models.CASCADE,
     )
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    subject = models.ForeignKey(
+        Subject,
+        related_name="courses",
+        on_delete=models.CASCADE,
+    )
+    image = models.ImageField(upload_to="courses/%Y/%m/%d")
+    duration = models.CharField(max_length=96, null=True, blank=True)
+    fees = models.DecimalField(max_digits=12, decimal_places=2)
+    level = models.CharField(max_length=96, null=True, blank=True)
+    instructor_based = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
 
 
 # Module Model
-class Module(models.Model):
+class Module(TitleDescriptionModel):
     course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    order = models.IntegerField(default=0)  # Order of the module in the course
+    order = OrderField(blank=True, for_fields=["course"])
 
     class Meta:
         ordering = ["order"]
@@ -43,6 +59,7 @@ class Content(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+    order = OrderField(blank=True, for_fields=["module"])
 
     class Meta:
         ordering = ["id"]
@@ -51,7 +68,7 @@ class Content(models.Model):
 # Text Model for storing text-based content
 class Text(models.Model):
     owner = models.ForeignKey(
-        User, related_name="text_related", on_delete=models.CASCADE
+        User, related_name="texts_related", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200, null=True, blank=True)
     content = models.TextField()
@@ -63,7 +80,7 @@ class Text(models.Model):
 # Video Model for storing video links (e.g., YouTube, Vimeo)
 class Video(models.Model):
     owner = models.ForeignKey(
-        User, related_name="video_related", on_delete=models.CASCADE
+        User, related_name="videos_related", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
     url = models.URLField()  # URL to the video (e.g., YouTube URL)
@@ -75,7 +92,7 @@ class Video(models.Model):
 # Image Model for storing images
 class Image(models.Model):
     owner = models.ForeignKey(
-        User, related_name="image_related", on_delete=models.CASCADE
+        User, related_name="images_related", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
     file = models.ImageField(upload_to="images")  # Image file upload
@@ -87,7 +104,7 @@ class Image(models.Model):
 # File Model for storing downloadable files (e.g., PDFs, documents)
 class File(models.Model):
     owner = models.ForeignKey(
-        User, related_name="file_related", on_delete=models.CASCADE
+        User, related_name="files_related", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
     file = models.FileField(upload_to="files")  # File upload (e.g., PDFs)
